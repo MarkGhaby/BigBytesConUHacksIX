39b8d0045df9f4d045b8fd970c3f40095f4f3b9c
andymiha <andreimihaescu@hotmail.com>
2025-02-01 17:16:31 -0500

    send to spotify button

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -9,25 +9,43 @@
         :key="index" 
         class="mb-2 flex justify-center"
       >
-        <div class="inline-block bg-stone-300 text-black rounded-lg p-2">
+        <div 
+          class="inline-block bg-stone-300 text-black rounded-lg p-2 w-4/5 break-words whitespace-pre-wrap"
+        >
           {{ msg }}
         </div>
       </div>
     </div>
 
-    <q-input
-      rounded
-      standout
-      clearable
-      counter
-      outlined
-      color="black"
-      bg-color="grey-4"
-      v-model="newMessage"
-      placeholder="Type to Journalify..."
-      @keyup.enter="sendMessage"
-      class="sticky bottom-0 w-full bg-stone-200 px-10 pb-10"
-    />
+    <div class="sticky bottom-0 w-full bg-stone-200 px-10 pb-6 flex items-center gap-4">
+  <!-- Text Input -->
+  <q-input
+    rounded
+    standout
+    clearable
+    counter
+    outlined
+    color="black"
+    bg-color="grey-4"
+    v-model="newMessage"
+    placeholder="Type to Journalify..."
+    @keyup.enter="sendMessage"
+    class="flex-1"
+  />
+
+  <!-- Send Button -->
+  <q-btn
+  rounded
+  text-color="white"
+  class="shrink-0 p-4"
+  style="background-color: #1DB954"
+  @click="sendMessage"
+>
+  <q-icon name="img:https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg" size="24px" />
+</q-btn>
+
+</div>
+
   </q-page>
 </template>
 
