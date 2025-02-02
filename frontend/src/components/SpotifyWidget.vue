de195d2fe5b5139192b39a22b2686f96f10808ca
andymiha <andreimihaescu@hotmail.com>
2025-02-02 01:22:06 -0500

    fixed widget styling

--- frontend/src/components/SpotifyWidget.vue
+++ frontend/src/components/SpotifyWidget.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex justify-center">
+  <div v-if="hasTracks" class="flex justify-center w-full">
     <q-carousel 
       animated 
       v-model="slide" 
       arrows 
       navigation 
-      infinite
-      class="rounded-lg w-4/5 bg-stone-300"
+      infinite 
+      class="rounded-lg w-4/5 bg-stone-200"
     >
       <q-carousel-slide
         v-for="(trackId, index) in journal.activeChat().trackId"
@@ -17,8 +17,10 @@
         <iframe
           v-if="trackId"
           :src="`https://open.spotify.com/embed/track/${trackId}`"
+          frameborder="0"
+          allowtransparency="true"
           allow="encrypted-media"
-          class="flex w-full h-full"
+          class="w-full h-full"
           @load="handleIframeLoad"
         ></iframe>
       </q-carousel-slide>
@@ -27,13 +29,14 @@
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue'
+import { ref, computed, defineEmits } from 'vue'
 import { useJournalStore } from 'src/stores/journal'
 
 const journal = useJournalStore()
-
 const slide = ref(0)
 const emit = defineEmits(['song-loaded'])
+
+const hasTracks = computed(() => journal.activeChat().trackId.length > 0)
 
 function handleIframeLoad() {
   emit('song-loaded')
